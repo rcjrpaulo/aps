740d6f24b23064da32905a422f96112e2c710b57
Paulo Cardoso <rcjrpaulo@gmail.com>
2019-09-11 18:27:48 -0300

    instalado electron e adicionado nav bar com titulo do app

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,12 @@
 <template>
   <q-page class="flex flex-center">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title class="text-center">
+          PopFilmes
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
     <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
   </q-page>
 </template>
