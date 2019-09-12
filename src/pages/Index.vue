ad7b6e046fe53ecb4744bfe962203076bf96fea3
Paulo Cardoso <rcjrpaulo@gmail.com>
2019-09-12 01:14:01 -0300

    agora traz os filmes e os titulos

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,10 +3,16 @@
     <q-header elevated>
       <q-toolbar>
         <q-toolbar-title class="text-center">
-          PopFilmes
+          {{ titulo }}
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
+    <q-body v-if="filmes.length" class="text-center">
+      <div :key="filme.id" v-for="(filme, chave) in filmes">
+        <img :src="'https://image.tmdb.org/t/p/w500/' + filme.poster_path" :alt="filme.original_title">
+        <h2>{{(chave+1) + '. ' + filme.original_title }}</h2>
+      </div>
+    </q-body>
   </q-page>
 </template>
 
@@ -18,8 +24,14 @@ export default {
   name: 'PageIndex',
   data: function () {
     return {
-      lorem: 'so vai'
+      titulo: 'PopFilmes',
+      filmes: []
     }
+  },
+  mounted () {
+    this.$axios.get('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f661bc3a7d6de05dd66918f3816b5c45').then((response) => {
+      this.filmes = response.data.results
+    })
   }
 }
 </script>
